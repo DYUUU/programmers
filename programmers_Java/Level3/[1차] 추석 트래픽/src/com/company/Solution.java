83efe40ce5da0c173ca0a9721b3441d53c07da08
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-03-07 17:41:09 +0900

    18.2점

--- programmers_Java/Level3/[1차] 추석 트래픽/src/com/company/Solution.java
+++ programmers_Java/Level3/[1차] 추석 트래픽/src/com/company/Solution.java
@@ -2,19 +2,16 @@ package com.company;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Comparator;
-import java.util.Date;
+import java.util.*;
 
 public class Solution {
     public int solution(String[] lines) throws ParseException {
-        int answer = 0;
+        ArrayList<Integer> answer = new ArrayList<>();
         // 날짜 포맷
         SimpleDateFormat trafficTimeFormat = new SimpleDateFormat("HH:mm:ss.SSS");
         // 날짜 계산기
         Calendar cal = Calendar.getInstance();
-
+        // 트래픽 리스트
         String[][] traffics = new String[lines.length][2];
 
         for (int i = 0; i < lines.length; i++) {
@@ -23,16 +20,22 @@ public class Solution {
             String[] processedStr = lines[i].split(" ");
 
             int index = processedStr[2].indexOf('.');
-            int milliSec = Integer.parseInt(processedStr[2].substring(index + 1, processedStr[2].length()));
-            int sec = Integer.parseInt(processedStr[2].substring(0, index));
+            int milliSec = 0;
+            int sec = 0;
+            if (index == -1) {
+                milliSec = 0;
+                sec = Integer.parseInt(processedStr[2]);
+            } else {
+                milliSec = Integer.parseInt(processedStr[2].substring(index + 1, processedStr[2].length()));
+                sec = Integer.parseInt(processedStr[2].substring(0, index));
+            }
 
             // 날짜 계산
             cal.setTime(trafficTimeFormat.parse(processedStr[1]));
             traffics[i][1] = trafficTimeFormat.format(cal.getTime());
             cal.add(Calendar.SECOND, -sec);
-            cal.add(Calendar.MILLISECOND, -milliSec);
+            cal.add(Calendar.MILLISECOND, -(milliSec-1));
             traffics[i][0] = trafficTimeFormat.format(cal.getTime());
-
         }
 
         // 정렬
@@ -46,6 +49,35 @@ public class Solution {
         for (int i = 0; i < traffics.length; i++) {
             System.out.println(Arrays.toString(traffics[i]));
         }
-        return answer;
+
+        Date min = trafficTimeFormat.parse(traffics[0][0]);
+        Date max = trafficTimeFormat.parse(traffics[traffics.length - 1][1]);
+        cal.setTime(min);
+
+        double timeLine = (max.getTime() - min.getTime()) / 1000;
+
+        for (int i = 0; i < timeLine; i++) {
+            answer.add(0);
+            for (int j = 0; j < traffics.length; j++) {
+                Date beforeTime = trafficTimeFormat.parse(traffics[j][0]);
+                Date scanner = cal.getTime();
+                if ((beforeTime.getTime() - scanner.getTime()) <= 0
+                ) {
+                    answer.set(i, answer.get(i) + 1);
+                }
+            }
+            cal.add(Calendar.SECOND, 1);
+        }
+
+        int result = 0;
+
+        for (int i : answer) {
+            if (i > result)
+                result = i;
+        }
+
+        System.out.println(result);
+
+        return result;
     }
 }
