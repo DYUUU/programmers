09350d4361d250c4d8a2fa07fb8844a68e0b1861
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-03-18 23:18:29 +0900

    24점

--- programmers_Java/Level2/괄호 변환/src/com/company/Solution.java
+++ programmers_Java/Level2/괄호 변환/src/com/company/Solution.java
@@ -1,27 +1,28 @@
 package com.company;
 
+import javax.lang.model.element.NestingKind;
+import javax.management.StringValueExp;
+
 public class Solution {
-    public String result = "";
 
-    public void DFS(StringBuilder u, StringBuilder v) {
-
-        if (String.valueOf(u).equals("()")) {
-            result += u;
-        } else {
+    public String DFS(StringBuilder u, StringBuilder v) {
+        int index = 0;
+        String strV = "";
+        String result;
+        if (v.length() != 0) {
+            index = div(String.valueOf(v));
+            strV = DFS(new StringBuilder(v.substring(0, index)), new StringBuilder(v.substring(index, v.length())));
+        }
+        if (u.charAt(u.length() - 1) == '(') {
             u.delete(0, 1);
             u.delete(u.length() - 1, u.length());
             String str = String.valueOf(u.reverse());
-            if(str.equals("()"))
-                result += '(' + str + ')';
-            else
-                result += '(' + String.valueOf(u.reverse()) + ')';
+            result= '(' + strV + ')' + str;
         }
-        if (String.valueOf(v).equals("()") || String.valueOf(v).equals("")) {
-            result += v;
-        } else {
-            int index = div(String.valueOf(v));
-            DFS(new StringBuilder(v.substring(0, index)), new StringBuilder(v.substring(index, v.length())));
+        else{
+            result = String.valueOf(u) + String.valueOf(v);
         }
+        return result;
     }
 
     public int div(String str) {
@@ -42,10 +43,9 @@ public class Solution {
     }
 
     public String solution(String p) {
+        String answer = "";
         int index = div(p);
-            DFS(new StringBuilder(p.substring(0, index)), new StringBuilder(p.substring(index, p.length())));
-        System.out.println(result);
-
-        return result;
+        answer = DFS(new StringBuilder(p.substring(0, index)), new StringBuilder(p.substring(index, p.length())));
+        return answer;
     }
 }
