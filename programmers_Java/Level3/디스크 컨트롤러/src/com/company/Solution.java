3749686bbfdd85780b11b7960628bbf4a5268cfa
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-03-24 13:14:35 +0900

    10점 . 모르겠음

--- programmers_Java/Level3/디스크 컨트롤러/src/com/company/Solution.java
+++ programmers_Java/Level3/디스크 컨트롤러/src/com/company/Solution.java
@@ -8,36 +8,46 @@ public class Solution {
         int sec = 0;
         int processing = 0;
 
-        Queue<int[]> jobs_queue = new LinkedList<>();
         PriorityQueue<Integer> waiting_jobs_queue = new PriorityQueue<Integer>();
         Queue<Integer> completed_jobs_queue = new LinkedList<>();
 
-        // jobs queue에 job 넣기
-        for (int i = 0; i < jobs.length; i++)
-            jobs_queue.add(jobs[i]);
-
+        Arrays.sort(jobs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] >= o2[0])
+                    return o1[1] - o2[1];
+                else
+                    return o1[0] - o2[0];
+            }
+        });
         // 첫 번재 process 추가
-        processing = (jobs_queue.poll()[1]);
+        processing = (jobs[0][1]);
+
 
         while (completed_jobs_queue.size() != jobs.length) {
             sec++;
             processing--;
-            for (int i = 0; i < jobs_queue.size(); i++) {
-                if (jobs_queue.peek()[0] == sec) {
-                    waiting_jobs_queue.add(jobs_queue.poll()[1]);
+            for (int i = 0; i < jobs.length; i++) {
+                if (jobs[i][0] == sec) {
+                    waiting_jobs_queue.add(jobs[i][1]);
 
                 }
             }
             if (processing == 0) {
-                completed_jobs_queue.add(sec);
-                if (waiting_jobs_queue.isEmpty() && jobs_queue.isEmpty())
+                completed_jobs_queue.add(sec - jobs[completed_jobs_queue.size()][0]);
+                if (waiting_jobs_queue.isEmpty())
                     break;
                 else
                     processing = waiting_jobs_queue.poll();
             }
         }
-
         System.out.println(completed_jobs_queue);
+
+        for (int i = 0; i < jobs.length; i++) {
+            answer += completed_jobs_queue.poll();
+        }
+        answer /= jobs.length;
+
 
         return answer;
     }
