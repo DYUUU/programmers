a688341ca1bfd0f6f172a66e495ae92a65b265d4
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-04-06 11:45:54 +0900

    해결 완료!

---
+++ programmers_Java/Level1/나누어 떨어지는 숫자 배열/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+
+        Solution sol = new Solution();
+        sol.solution(arr,divisor);
+
+    }
+}
