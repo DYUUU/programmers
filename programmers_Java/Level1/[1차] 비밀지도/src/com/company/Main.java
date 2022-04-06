4b0845f79431f06fbc843c7fa0bd594b2199eaf4
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-04-06 14:49:32 +0900

    해결 완료

---
+++ programmers_Java/Level1/[1차] 비밀지도/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n = 5;
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+
+        Solution sol = new Solution();
+        sol.solution(n,arr1,arr2);
+    }
+}
