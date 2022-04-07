0176957dbb71b5423efeabe0b57e1438806e84ff
HellSkk <63494210+DYUUU@users.noreply.github.com>
2022-04-07 09:17:14 +0900

    해결 완료

--- programmers_Java/Level1/크레인 인형뽑기 게임/src/com/company/Main.java
+++ programmers_Java/Level1/크레인 인형뽑기 게임/src/com/company/Main.java
@@ -10,7 +10,7 @@ public class Main {
                 {0,2,5,0,1},
                 {4,2,4,4,2},
                 {3,5,1,3,1}};
-        int[] moves = {1,1,1,1,1,1};
+        int[] moves = {1,5,3,5,1,2,1,4};
         Solution sol = new Solution();
 
         sol.solution(board,moves);
